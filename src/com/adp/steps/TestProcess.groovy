ffd8620bb497f1efc17af15a5f96d664fc666dee
Steven <stevendeal@gmail.com>
2019-06-25 11:07:16 -0400

    Create TestProcess.groovy

---
+++ src/com/adp/steps/TestProcess.groovy
@@ -0,0 +1,15 @@
+package com.adp.steps
+
+//def call() {
+//    return
+//    // ----------------------------------------------------------------------------------------------------------
+//    // *** Process DEPLOY to DEV - If NOT Release or Hotfix Branch ***
+//    // ----------------------------------------------------------------------------------------------------------
+//    artifacts.deploy("Dev")
+//}
+
+def myCall() {
+  echo "hello my world"
+}
+
+return this
