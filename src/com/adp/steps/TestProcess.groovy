18dd1591aa2107870d1caf687b5c9a72449a1b30
Steven <stevendeal@gmail.com>
2019-06-25 11:22:18 -0400

    Update TestProcess.groovy

--- src/com/adp/steps/TestProcess.groovy
+++ src/com/adp/steps/TestProcess.groovy
@@ -1,16 +1,7 @@
 package com.adp.steps
 
-//def call() {
-//    return
-//    // ----------------------------------------------------------------------------------------------------------
-//    // *** Process DEPLOY to DEV - If NOT Release or Hotfix Branch ***
-//    // ----------------------------------------------------------------------------------------------------------
-//    artifacts.deploy("Dev")
-//}
-
  static def call() {
-    echo "This is how methods work in groovy";
-    echo "This is an example of a simple method";
+    return 'I was called'
  } 
 
 return this
