68319d5f320cbbb44e1771935e6cbb7bbf83cac3
Steven <stevendeal@gmail.com>
2019-07-17 17:34:48 -0400

    Update getTriggers.groovy

--- vars/getTriggers.groovy
+++ vars/getTriggers.groovy
@@ -1,4 +1,8 @@
 def call(trigger_cause) {
-   env.lunch = "candy"
-   return env //ourCount
+   trigger_cause.each {
+      def s = it.event
+      def x = s.split(":")
+      env[x[0]] = x[1]
+      //env.lunch = candy
+   return env
 }
