6d7bec1525838df4bf468232285a5827fceb6f1a
Steven <stevendeal@gmail.com>
2019-07-17 17:51:14 -0400

    Update getTriggers.groovy

--- vars/getTriggers.groovy
+++ vars/getTriggers.groovy
@@ -1,6 +1,6 @@
 def call(trigger_cause) {
    trigger_cause.each {
-      echo "Trigger event: $it.event"
+      echo "Trigger event: $it.event.event"
    }
    env.lunch = "brussels sprouts"
    return env
