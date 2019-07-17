2463f59f149ac2a5c45c9732d44b965b99f34f6c
Steven <stevendeal@gmail.com>
2019-07-17 15:00:04 -0400

    Update Triggers.groovy

--- src/com/acme/Triggers.groovy
+++ src/com/acme/Triggers.groovy
@@ -1,10 +1,11 @@
 package com.acme
 
 def getTriggers(trigger_cause) {
-   //echo "My trigger cause: ${trigger_cause}"
-   //echo "Event Key: ${trigger_cause[0].event}"
-   //echo "We had a total of ${trigger_cause.size()}"
-   return "2"
+   echo "My trigger cause: ${trigger_cause}"
+   echo "Event Key: ${trigger_cause[0].event}"
+   echo "We had a total of ${trigger_cause.size()}"
+   def ourCount = trigger_cause.size()
+   return ourCount
 }
 
 return this
