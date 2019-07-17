fafef2b9ac543c2a80aade0675a6553957c6f376
Steven <stevendeal@gmail.com>
2019-07-17 14:14:03 -0400

    Create Triggers.groovy

---
+++ src/com/acme/Triggers.groovy
@@ -0,0 +1,10 @@
+package com.acme
+
+def getTriggers(trigger_cause) {
+   echo "My trigger cause: ${trigger_cause}"
+   echo "Event Key: ${trigger_cause[0].event}"
+   echo "We had a total of ${trigger_cause.size()}"
+   return "2"
+}
+
+return this
