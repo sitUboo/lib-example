a2fb0c0f359db8cc190e873e7ba505e702826bfc
Steven <stevendeal@gmail.com>
2019-07-17 17:54:19 -0400

    Update getTriggers.groovy

--- vars/getTriggers.groovy
+++ vars/getTriggers.groovy
@@ -1,6 +1,9 @@
 def call(trigger_cause) {
    trigger_cause.each {
       echo "Trigger event: $it.event.event"
+      str = it.event.event
+      def x[] = str.split(":")
+      echo "Bits $x[0] and $x[1]"
    }
    env.lunch = "brussels sprouts"
    return env
