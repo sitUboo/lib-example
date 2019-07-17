59e4df1ee1f744e00cea18bbdd049bf4dcf06658
Steven <stevendeal@gmail.com>
2019-07-17 13:40:12 -0400

    Update getTriggers.groovy

--- vars/getTriggers.groovy
+++ vars/getTriggers.groovy
@@ -3,5 +3,6 @@ def call() {
    echo "My trigger cause: ${trigger_cause}"
    echo "Event Key: ${trigger_cause[0].event}"
    echo "We had a total of ${trigger_cause.size()}"
-   return trigger_cause.size()
+   //return trigger_cause.size()
+   return 2
 }
