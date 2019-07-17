1aaa25560d51b7778816c06eabd6bdd4230297b0
Steven <stevendeal@gmail.com>
2019-07-17 18:05:18 -0400

    Update getTriggers.groovy

--- vars/getTriggers.groovy
+++ vars/getTriggers.groovy
@@ -1,6 +1,8 @@
 def call(trigger_cause) {
    trigger_cause.each {
-      echo "Trigger event: $it.event.event.lunch"
+      echo "Trigger event: $it.event.event"
+      myEvent = "$it.event.event"
+      echo myEvent
       //str = it.event.event
       //x = str.split(",")
       //x = "$it.event.event.split(':')"
