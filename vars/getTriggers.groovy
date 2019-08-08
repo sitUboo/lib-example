2557400caeb77ca98de285b90e4a04f80f03489a
Steven <stevendeal@gmail.com>
2019-08-08 13:27:04 -0400

    Update getTriggers.groovy

--- vars/getTriggers.groovy
+++ vars/getTriggers.groovy
@@ -1,8 +1,8 @@
 def call(trigger_cause) {
    echo "Evaluating trigger_cause"
-   trigger_cause.each {
-      echo "Trigger event: $it"
-   }
+  // trigger_cause.each {
+  //    echo "Trigger event: $it"
+  // }
    //env["lunch"] = "brussels sprouts"
    env["lunch"] = "candy"
 }
