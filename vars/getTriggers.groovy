64d219c345fb71daeae7be5990b279a41b2ea3dc
Steven <stevendeal@gmail.com>
2019-08-13 10:52:59 -0400

    Update getTriggers.groovy

--- vars/getTriggers.groovy
+++ vars/getTriggers.groovy
@@ -1,5 +1,7 @@
 def call(trigger_cause) {
-   echo "Evaluating trigger_cause"
+   echo "Evaluating trigger_cause: " + trigger_cause
+   
+   echo trigger_cause.repository.name
   // trigger_cause.each {
   //    echo "Trigger event: $it"
   // }
