1b82ee99f766839ccc5e1d3ae9d06430fbb4a931
Steven <stevendeal@gmail.com>
2019-08-23 10:15:49 -0400

    Delete isValid.groovy

--- vars/isValid.groovy
+++
@@ -1,9 +0,0 @@
-def call(start_time, end_time) {
-   try{
-     def sDate = new Date().parse('dd.MM.yyyy HH:mm:ss',start_time)
-     def eDate = new Date().parse('dd.MM.yyyy HH:mm:ss',end_time)
-   }catch(Exception e){
-     return false
-   }
-   return true
-}
