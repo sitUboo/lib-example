585b640d893bd209f2cd931597cd9a369960b961
Steven <stevendeal@gmail.com>
2019-10-02 12:14:20 -0400

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -13,9 +13,10 @@ def call(Map config) {
                 script{
                     git 'git@github.com:sitUboo/choices.git'
                     def foundFiles = findFiles(glob: '*.txt')
-                    for (int i = 0; i < foundFiles.size(); i++) {
-                        echo foundFiles[i].getName()
-                    }
+                    env.FILE_TO_TEST = input message: 'User input required', ok: 'Input File',
+                            parameters: [choice(name: 'FILE_TO_TEST', choices: foundFiles, description: 'What file to use?')]
+                }
+                echo "${env.FILE_TO_TEST}"
                     //deleteDir()
                 }
             }
