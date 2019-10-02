2c19f126e9f97c5174a622608f62061350d6f2e7
Steven <stevendeal@gmail.com>
2019-10-02 12:03:51 -0400

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -14,7 +14,7 @@ def call(Map config) {
                     git 'git@github.com:sitUboo/choices.git'
                     def foundFiles = findFiles(glob: '*.txt')
                     for (int i = 0; i < foundFiles.size(); i++) {
-                        echo i
+                        echo foundFiles[i]
                     }
                     //deleteDir()
                 }
