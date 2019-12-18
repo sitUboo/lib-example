307eaaea39f1bdc04020c67e102717a8895611c6
Steven <stevendeal@gmail.com>
2019-12-18 15:25:42 -0500

    Update getMvnPod.groovy

--- vars/getMvnPod.groovy
+++ vars/getMvnPod.groovy
@@ -1,4 +1,5 @@
-def call() {
+def call(Map config) {
+  
   def podYaml = """
 apiVersion: v1
 kind: Pod
@@ -8,6 +9,10 @@ spec:
     image: maven:3.3.9-jdk-8-alpine
     command: ['cat']
     tty: true
+  - name: ${config.name}
+    image: ${config.image}
+    command: ['${config.command}']
+    tty: ${config.tty}
 """
   return podYaml.toString()
 }
