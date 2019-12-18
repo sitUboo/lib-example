f42338cf51538dd4beb3d2504ad042591ea8aae2
Steven <stevendeal@gmail.com>
2019-12-18 10:43:01 -0500

    Create createPod.groovy

---
+++ vars/createPod.groovy
@@ -0,0 +1,14 @@
+def call() {
+    kubernetes {
+      yaml """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: maven
+    image: maven:3.3.9-jdk-8-alpine
+    command: ['cat']
+    tty: true
+"""
+    }
+}
