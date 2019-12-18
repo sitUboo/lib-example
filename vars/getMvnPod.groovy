8ad34da55e8e74fa56155ec6defb0b069e8349db
Steven <stevendeal@gmail.com>
2019-12-18 11:10:16 -0500

    Create getMvnPod.groovy

---
+++ vars/getMvnPod.groovy
@@ -0,0 +1,13 @@
+def call() {
+  def podYaml = """
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: maven
+    image: maven:3.3.9-jdk-8-alpine
+    command: ['cat']
+    tty: true
+"""
+  return podYaml.toString()
+}
